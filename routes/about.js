d6970335f8b7f97614d9d1070b6c588f21bc3b76
anandvp92 <anandvp92@gmail.com>
2024-08-19 00:38:58 +0530

    updated the css: :

--- routes/about.js
+++ routes/about.js
@@ -5,7 +5,8 @@ var router = express.Router();
 
 const products = [
     {name:"nokia",brand:"nokia",catogoray:"phone" ,price:35000, pic:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/v/e/a/-original-imahfk4xuk7ntphs.jpeg?q=80"},
-    {name:"nokia",brand:"nokia",catogoray:"phone" ,price:35000, pic:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/d/h/q/m6-pro-5g-mzb0eprin-poco-original-imags3e7vewsafst.jpeg?q=70"},
+    {name:"nokia",brand:"nokia",catogoray:"phone" ,price:35000,
+         pic:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70"},
     {name:"nokia",brand:"nokia",catogoray:"phone" ,price:35000, pic:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/d/h/q/m6-pro-5g-mzb0eprin-poco-original-imags3e7vewsafst.jpeg?q=70"},
     {name:"nokia",brand:"nokia",catogoray:"phone" ,price:35000, pic:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/d/h/q/m6-pro-5g-mzb0eprin-poco-original-imags3e7vewsafst.jpeg?q=70"},
     {name:"nokia",brand:"nokia",catogoray:"phone" ,price:35000, pic:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/d/h/q/m6-pro-5g-mzb0eprin-poco-original-imags3e7vewsafst.jpeg?q=70"},
